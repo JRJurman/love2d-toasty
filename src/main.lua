1e39c98e2b888a9e587b6ebbf7392c94b2e483de
Jesse Jurman <j.r.jurman@gmail.com>
2026-07-23 01:42:10 -0400

    dynamic readout font size based on text length

--- src/main.lua
+++ src/main.lua
@@ -623,6 +623,12 @@ function loadSeed()
 	math.randomseed(gameSeed)
 end
 
+function getReadoutFontSize(readoutSize)
+	local scale = -0.15
+	local offset = 68
+	return scale*readoutSize + offset
+end
+
 function love.load()
 	async(routines, function()
 		startNewGame()
@@ -835,16 +841,15 @@ function love.draw()
 	-- readout text
 	love.graphics.setColor(16/256, 20/256, 31/256)
 
-	-- change font size based on has started (the first modal needs a lot of space)
-	if hasStarted == false then
-		love.graphics.setFont(getFont(39))
-	else
-		love.graphics.setFont(getFont(46))
-	end
 	local readoutText = selectionText
 	if isAnimating then
 		readoutText = animationText
 	end
+
+	-- change font size based on the amount of text
+	local readoutFontSize = getReadoutFontSize(#readoutText)
+	love.graphics.setFont(getFont(readoutFontSize))
+
 	love.graphics.printf(readoutText, ui.readout.x + 15, ui.readout.y, ui.readout.width - 30, 'center')
 
 	-- draw the chef
