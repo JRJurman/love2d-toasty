1f250c47d19cebd5a6f3bbc273e8df7905bf08ec
Jesse Jurman <j.r.jurman@gmail.com>
2026-07-25 01:52:09 -0400

    count pita in hand when calculating remaining bread

--- src/main.lua
+++ src/main.lua
@@ -424,11 +424,12 @@ function updateSelectionAfterPlayOrDraw(overriddenEmptyPlateAnimationText)
 
 	local breadInDeck = countValueInTopOfPile(drawPile, #drawPile, 1)
 	local pitaInDeck = countValueInTopOfPile(drawPile, #drawPile, 19)
+	local pitaInHand = countValueInTopOfPile(hand, #hand, 19)
 	local currentPlateRawScore = getRawScoreForPlate(currentPlate)
 
 	-- if we are out of bread (and pita), and have no plate, end the game
 	-- alternatively if we have no cards in deck (and the hand is empty), end the game
-	local outOfBread = breadInDeck == 0 and pitaInDeck == 0 and #currentPlate == 0
+	local outOfBread = breadInDeck == 0 and pitaInDeck == 0 and pitaInHand == 0 and #currentPlate == 0
 	local outOfCards = #drawPile == 0 and handIsEmpty
 
 	if outOfBread or outOfCards then
