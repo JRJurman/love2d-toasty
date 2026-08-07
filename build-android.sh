c182195a927c3492c35db8b91fa78c558e0dede4
Jesse Jurman <j.r.jurman@gmail.com>
2026-08-06 23:12:42 -0400

    aab bundle config, API 36

--- build-android.sh
+++ build-android.sh
@@ -2,6 +2,7 @@
 cd android
 case "$1" in
   --apk) ./gradlew assembleEmbedNoRecordRelease && cp app/build/outputs/apk/embedNoRecord/release/app-embed-noRecord-release.apk ../Toasty-Android.apk ;;
+  --aab) ./gradlew bundleEmbedNoRecordRelease && cp app/build/outputs/bundle/embedNoRecordRelease/app-embed-noRecord-release.aab ../Toasty-Android.aab ;;
   --device) ./gradlew installEmbedNoRecordDebug ;;
   *) echo 'provide target'
 esac
